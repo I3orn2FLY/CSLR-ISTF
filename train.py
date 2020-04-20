ccc46318f240fdfcea8874529a5e2f3fb68dbb36
Kenessary Koishybay <kenessary.koishybay@nu.edu.kz>
2020-04-20 14:37:14 +0600

    small bug fix

--- train.py
+++ train.py
@@ -19,79 +19,82 @@ def train(mode, model, loaded, model_path, vocab, datasets):
     loss_fn = nn.CTCLoss(zero_infinity=True)
 
     best_wer, best_wer_file, criterion_phase = get_wer_info(mode, loaded)
+    try:
+        for epoch in range(1, n_epochs + 1):
+            print("Epoch", epoch)
+            for phase in ['Train', 'Val']:
+                if phase == 'Train':
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()
 
-    for epoch in range(1, n_epochs + 1):
-        print("Epoch", epoch)
-        for phase in ['Train', 'Val']:
-            if phase == 'Train':
-                model.train()  # Set model to training mode
-            else:
-                model.eval()
+                dataset = datasets[phase]
+                n_batches = dataset.start_epoch()
+                losses = []
+                hypes = []
+                gts = []
 
-            dataset = datasets[phase]
-            n_batches = dataset.start_epoch()
-            losses = []
-            hypes = []
-            gts = []
+                with torch.set_grad_enabled(phase == "Train"):
+                    pp = ProgressPrinter(n_batches, 25)
+                    for i in range(n_batches):
+                        optimizer.zero_grad()
+                        X_batch, Y_batch, Y_lens = dataset.get_batch(i)
+                        X_batch = X_batch.to(DEVICE)
+                        L = X_batch.size()[-2]
+                        if L < 4 * Y_lens.max().item():
+                            batch = dataset.batches[i]
+                            paths = [dataset.X[k] for k in batch]
+                            print(paths)
+                            continue
 
-            with torch.set_grad_enabled(phase == "Train"):
-                pp = ProgressPrinter(n_batches, 25)
-                for i in range(n_batches):
-                    optimizer.zero_grad()
-                    X_batch, Y_batch, Y_lens = dataset.get_batch(i)
-                    X_batch = X_batch.to(DEVICE)
-                    L = X_batch.size()[-2]
-                    if L < 4 * Y_lens.max().item():
-                        batch = dataset.batches[i]
-                        paths = [dataset.X[k] for k in batch]
-                        print(paths)
-                        continue
+                        preds = model(X_batch).log_softmax(dim=2)
+                        T, N, V = preds.shape
+                        X_lens = torch.full(size=(N,), fill_value=T, dtype=torch.int32)
+                        loss = loss_fn(preds, Y_batch, X_lens, Y_lens)
 
-                    preds = model(X_batch).log_softmax(dim=2)
-                    T, N, V = preds.shape
-                    X_lens = torch.full(size=(N,), fill_value=T, dtype=torch.int32)
-                    loss = loss_fn(preds, Y_batch, X_lens, Y_lens)
+                        if torch.isnan(loss):
+                            print("NAN!!")
 
-                    if torch.isnan(loss):
-                        print("NAN!!")
+                        losses.append(loss.item())
 
-                    losses.append(loss.item())
+                        if phase == "Train":
+                            loss.backward()
+                            optimizer.step()
 
-                    if phase == "Train":
-                        loss.backward()
-                        optimizer.step()
+                        out_sentences = predict_glosses(preds, decoder=None)
+                        gts += [y for y in Y_batch.view(-1).tolist() if y != 0]
 
-                    out_sentences = predict_glosses(preds, decoder=None)
-                    gts += [y for y in Y_batch.view(-1).tolist() if y != 0]
+                        for sentence in out_sentences:
+                            hypes += sentence
 
-                    for sentence in out_sentences:
-                        hypes += sentence
+                        if i == 0:
+                            pred = " ".join(vocab.decode(out_sentences[0]))
+                            gt = Y_batch[0][:Y_lens[0]].tolist()
+                            gt = " ".join(vocab.decode(gt))
+                            print(phase, 'Ex. [' + pred + ']', '[' + gt + ']')
 
-                    if i == 0:
-                        pred = " ".join(vocab.decode(out_sentences[0]))
-                        gt = Y_batch[0][:Y_lens[0]].tolist()
-                        gt = " ".join(vocab.decode(gt))
-                        print(phase, 'Ex. [' + pred + ']', '[' + gt + ']')
+                        if SHOW_PROGRESS:
+                            pp.show(i)
 
                     if SHOW_PROGRESS:
-                        pp.show(i)
+                        pp.end()
 
-                if SHOW_PROGRESS:
-                    pp.end()
+                hypes = "".join([chr(x) for x in hypes])
+                gts = "".join([chr(x) for x in gts])
+                phase_wer = Lev.distance(hypes, gts) / len(gts) * 100
 
-            hypes = "".join([chr(x) for x in hypes])
-            gts = "".join([chr(x) for x in gts])
-            phase_wer = Lev.distance(hypes, gts) / len(gts) * 100
+                phase_loss = np.mean(losses)
+                print(phase, "WER:", phase_wer, "Loss:", phase_loss)
 
-            phase_loss = np.mean(losses)
-            print(phase, "WER:", phase_wer, "Loss:", phase_loss)
+                if phase == criterion_phase and phase_wer < best_wer:
+                    best_wer = phase_wer
+                    save_model(model, model_path, best_wer, best_wer_file)
 
-            if phase == criterion_phase and phase_wer < best_wer:
-                best_wer = phase_wer
-                save_model(model, model_path, best_wer, best_wer_file)
-
-        print()
-        print()
+            print()
+            print()
+    except KeyboardInterrupt:
+        pass
+    print("Training complete:", "Best WER:", best_wer)
 
 
 if __name__ == "__main__":
