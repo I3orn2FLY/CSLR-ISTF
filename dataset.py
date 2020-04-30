2267278ab55f40c462560d5c94e55dbf528e26d6
Kenessary Koishybay <kenessary.koishybay@nu.edu.kz>
2020-05-01 02:35:36 +0600

    gr dataset

--- dataset.py
+++ dataset.py
@@ -587,21 +587,74 @@ class End2EndTempFusionDataset(End2EndDataset):
 
 class GR_dataset():
     def __init__(self, split, batch_size):
-        self.df = pd.read_csv(os.path.join(ANNO_DIR, "gloss_" + split + ".csv"))
 
         self.batch_size = batch_size
         self.mean = np.array([0.43216, 0.394666, 0.37645], dtype=np.float32)
         self.std = np.array([0.22803, 0.22145, 0.216989], dtype=np.float32)
+
         self.batches = [[]]
 
-    def get_sample(self, i):
-        row = self.df.iloc[i]
-        y = int(row.gloss_idx)
-        video_dir = os.path.join(GLOSS_DATA_DIR, row.folder)
-        images = []
+        self.build_dataset(split)
 
-        image_files = list(glob.glob(video_dir))
-        image_files.sort()
+    def build_dataset(self, split):
+
+        prefix_dir = os.sep.join([VARS_DIR, "GR_Dataset", SOURCE, END2END_TRAIN_MODE])
+
+        X_path = os.sep.join([prefix_dir, "X_" + split + ".pkl"])
+        Y_path = os.sep.join([prefix_dir, "Y_" + split + ".pkl"])
+        X_lens_path = os.sep.join([prefix_dir, "X_lens_" + split + ".pkl"])
+
+        if os.path.exists(X_path) and os.path.exists(Y_path) and os.path.exists(X_lens_path):
+            with open(X_path, 'rb') as f:
+                self.X = pickle.load(f)
+
+            with open(Y_path, 'rb') as f:
+                self.Y = pickle.load(f)
+
+            with open(X_lens_path, 'rb') as f:
+                self.X_lens = pickle.load(f)
+
+            print("GR", split, "dataset loaded")
+            return
+
+        print("Building GR", split, "dataset")
+        df = pd.read_csv(os.path.join(ANNO_DIR, "gloss_" + split + ".csv"))
+        self.X = []
+        self.X_lens = []
+        self.Y = []
+        pp = ProgressPrinter(df.shape[0], 25)
+        for i in range(df.shape[0]):
+            row = df.iloc[i]
+            video_dir = os.path.join(GLOSS_DATA_DIR, row.folder)
+            image_files = list(glob.glob(video_dir))
+            image_files.sort()
+
+            self.X.append(image_files)
+            self.X_lens.append(len(image_files))
+            self.Y.append(int(row.gloss_idx))
+
+            if SHOW_PROGRESS:
+                pp.show(i)
+
+        if SHOW_PROGRESS:
+            pp.end()
+
+        if not os.path.exists(prefix_dir):
+            os.makedirs(prefix_dir)
+
+        with open(X_path, 'wb') as f:
+            pickle.dump(self.X, f)
+
+        with open(Y_path, 'wb') as f:
+            pickle.dump(self.Y, f)
+
+        with open(X_lens_path, 'wb') as f:
+            pickle.dump(self.X_lens, f)
+
+    def get_sample(self, i):
+        y = self.Y[i]
+        image_files = self.X[i]
+        images = []
 
         for img_file in image_files:
             img = cv2.imread(img_file)
@@ -615,15 +668,32 @@ class GR_dataset():
 
         return x, y
 
-    def start_epoch(self):
-        idxs = list(range(self.df.shape[0]))
-        np.random.shuffle(idxs)
+    def start_epoch(self, shuffle=True):
+        len_table = dict()
+
+        for i, length in enumerate(self.X_lens):
+            if length in len_table:
+                len_table[length].append(i)
+            else:
+                len_table[length] = [i]
+
         self.batches = []
-        s = 0
-        while s < len(idxs):
-            e = min(s + self.batch_size, len(idxs))
-            self.batches.append(idxs[s:e])
-            s += self.batch_size
+        lenghts = list(len_table)
+
+        if shuffle:
+            random.shuffle(lenghts)
+
+        for l in lenghts:
+            idxs = len_table[l]
+            if shuffle:
+                random.shuffle(idxs)
+            s = 0
+            while (s < len(idxs)):
+                e = min(s + self.batch_size, len(idxs))
+
+                self.batches.append(idxs[s:e])
+
+                s += self.batch_size
 
         return len(self.batches)
 
